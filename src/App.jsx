0f04132ffcd3a2a27c8249a056e09026a79ac2e0
bouhourd <killianbouhourd@gmail.com>
2024-10-23 14:03:24 +0200

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import React from 'react'
+import Header from './Header'
+
 import './App.css'
+
+
 
 function App() {
   const articles = [
-    {id : 1, title:"react"},
-    {id : 2, title:"Angular"},
-    {id : 3, title:"Vue"},
+    {id:1 ,title: 'React'},
+    {id:2 ,title: 'Angular'},
+    {id:3, title: 'Vue'},
   ];
 
+  const title = "Titre de la page"
+  
   return (
-      <div>
-        <h1>Articles</h1>
-        <ul>
-          {articles.map((articles) =>(
-            <li key={articles.id}>{articles.title}</li>
-          ))}
-        </ul>
-      </div>
-  );
+    <div>
+      <Header />
+      <h1>{title}</h1>
+      <ul>
+        {articles.map((article) => (
+          <li key={article.id}>{article.title}</li>
+        ))}
+      </ul>
+    </div>
+  )
 }
 
-export default App
+export default App;
---
+++ src/Header.jsx
@@ -0,0 +1,18 @@
+
+
+function Header() {
+    return (
+      <header>
+        <h1>Epsi React</h1>
+        <nav>
+          <ul>
+            <li>Home</li>
+            <li>Articles</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+      </header>
+    );
+  }
+  
+  export default Header;
